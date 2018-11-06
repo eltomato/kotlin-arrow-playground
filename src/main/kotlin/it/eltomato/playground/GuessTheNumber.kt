d2268b53afe058927d2790ea98ac56ae899fe765
Tommaso Previero <t.previero@gmail.com>
2018-11-06 22:52:38 +0100

    Some hypothesis on how to implement the keepAsking function.

--- src/main/kotlin/it/eltomato/playground/GuessTheNumber.kt
+++ src/main/kotlin/it/eltomato/playground/GuessTheNumber.kt
@@ -1,7 +1,9 @@
 package it.eltomato.playground
 
 import arrow.core.Try
+import arrow.core.getOrElse
 import arrow.effects.IO
+import arrow.effects.liftIO
 import it.eltomato.playground.GuessResult.GuessCorrect
 import it.eltomato.playground.GuessResult.GuessIncorrect
 import java.util.*
@@ -17,26 +19,60 @@ fun main(vararg args: String) {
 }
 
 fun keepAsking(guessNumber: IO<GuessResult>): IO<GuessResult> {
-    return guessNumber.attempt().flatMap {
-        it.fold(
-            { writeException(it).flatMap { keepAsking(guessNumber) } },
-            { guessResult ->
-                when (guessResult) {
-                    is GuessIncorrect -> keepAsking(guessNumber)
-                    else -> {
-                        writeNailed().map { guessResult }
-                    }
-                }
-            })
-    }
+//    return guessNumber.attempt().flatMap {
+//        it.fold(
+//            { writeException(it).flatMap { keepAsking(guessNumber) } },
+//            { guessResult ->
+//                writeAnalysis(guessResult).flatMap {
+//                    when (guessResult) {
+//                        is GuessIncorrect -> keepAsking(guessNumber)
+//                        else -> guessResult.liftIO()
+//                    }
+//                }
+//            })
+//    }
+//    return guessNumber.attempt().flatMap { guessResult ->
+//        when (guessResult) {
+//            is Either.Left -> writeException(guessResult.a).flatMap { keepAsking(guessNumber) }
+//            is Either.Right -> writeAnalysis(guessResult.b).flatMap {
+//                if (guessResult.b is GuessCorrect) {
+//                    GuessCorrect.liftIO()
+//                } else {
+//                    keepAsking(guessNumber)
+//                }
+//            }
+//        }
+//    }
+//    return guessNumber.attempt()
+//        .flatMap { attempt ->
+//            attempt.fold(
+//                { writeException(it) },
+//                { writeAnalysis(it) }
+//            ).flatMap {
+//                if (attempt is Either.Right && attempt.b == GuessCorrect) {
+//                    GuessCorrect.liftIO()
+//                } else {
+//                    keepAsking(guessNumber)
+//                }
+//            }
+//        }
+    return guessNumber.attempt()
+        .flatMap { attempt ->
+            attempt.fold(
+                { writeException(it) },
+                { writeAnalysis(it) }
+            ).flatMap {
+                attempt.toOption()
+                    .filter { it == GuessCorrect }
+                    .map { it.liftIO() }
+                    .getOrElse { keepAsking(guessNumber) }
+            }
+
+        }
 }
 
 fun writeException(it: Throwable): IO<Unit> = IO {
     println("Woops...${it::class.java.name} ${it.message}")
-}
-
-fun writeNailed(): IO<Unit> = IO {
-    println("You nailed it!")
 }
 
 fun guessNumber(numberToGuess: Int): IO<GuessResult> =
@@ -44,16 +80,14 @@ fun guessNumber(numberToGuess: Int): IO<GuessResult> =
         .flatMap { readAString() }
         .flatMap { parseNumber(it).toIO() }
         .map { analyzeGuess(it, numberToGuess) }
-        .flatMap { writeAnalysis(it) }
 
 fun writeIntroduction(limit: Int): IO<Unit> = IO { println("You have to guess a number between 0 and $limit") }
 
-fun writeAnalysis(guessResult: GuessResult): IO<GuessResult> = IO {
+fun writeAnalysis(guessResult: GuessResult): IO<Unit> = IO {
     when (guessResult) {
         is GuessIncorrect -> println("Wrong number!")
         is GuessCorrect -> println("That's the right number!")
     }
-    guessResult
 }
 
 fun askForNumber(): IO<Unit> = IO { print("Take a guess:") }
